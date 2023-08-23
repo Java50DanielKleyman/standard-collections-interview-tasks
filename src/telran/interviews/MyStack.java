57ebb5e96938175b2535357600796160f4b7615e
Daniel Kleyman <denny_klm1@yahoo.com>
2023-08-23 19:50:14 +0300

    HW-26

--- src/telran/interviews/MyStack.java
+++ src/telran/interviews/MyStack.java
@@ -1,52 +1,62 @@
 package telran.interviews;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.NoSuchElementException;
-import java.util.PriorityQueue;
 
 public class MyStack<T> {
-	private LinkedList<T> myLinkedList = new LinkedList<T>();
-	
-	private PriorityQueue<T> myPriorityQueue; 
-	
+	private LinkedList<T> myLinkedList;
+	private ArrayList<T> maxValues;
+	private Comparator<T> comparator;
+
 	public MyStack(Comparator<T> comp) {
-		  myPriorityQueue = new PriorityQueue<>(comp);
-		
+		comparator = comp;		
+		myLinkedList = new LinkedList<T>();
+		maxValues = new ArrayList<T>();
+
 	}
+
 	@SuppressWarnings("unchecked")
 	public MyStack() {
-		this((Comparator<T>) Comparator.reverseOrder());
+		this((Comparator<T>) Comparator.naturalOrder());
 	}
-	
-  public void push(T element) {
-	  myLinkedList.add(element);
-	  myPriorityQueue.add(element);
-  }
-  public T pop() {
-	 if(isEmpty()) {
-		 throw new NoSuchElementException("empty stack");
-	 }	 
-	 T removedElement = myLinkedList.removeLast();
-	  myPriorityQueue.remove(removedElement);
-	 
-	  return removedElement;
-  }
-  public boolean isEmpty() {
-	  if(myLinkedList.size() == 0 && myPriorityQueue.size()==0) {
-		  return true;
-	  }
-	  return false;
-  }
-  public T getMax() {
-	  if(isEmpty()) {
-			 throw new NoSuchElementException("empty stack");
-		 }	  
-	 
-	  return myPriorityQueue.peek();
-  }
-  @SuppressWarnings("unchecked")
-public T [] toArray() {
-	  return (T[]) myLinkedList.toArray();
-  }
+
+	public void push(T element) {
+		myLinkedList.add(element);
+		if (maxValues.isEmpty() || comparator.compare(element, maxValues.get(maxValues.size() - 1)) >= 0) {
+			maxValues.add(element);
+		} else {
+			maxValues.add(maxValues.get(maxValues.size() - 1));
+		}		
+	}
+
+	public T pop() {
+		if (isEmpty()) {
+			throw new NoSuchElementException("empty stack");
+		}
+		T removedElement = myLinkedList.removeLast();
+		maxValues.remove(maxValues.size() - 1);		
+		return removedElement;
+	}
+
+	public boolean isEmpty() {
+		if (myLinkedList.size() == 0 && maxValues.size() == 0) {
+			return true;
+		}
+		return false;
+	}
+
+	public T getMax() {
+		if (isEmpty()) {
+			throw new NoSuchElementException("empty stack");
+		}
+
+		return maxValues.get(maxValues.size() - 1);
+	}
+
+	@SuppressWarnings("unchecked")
+	public T[] toArray() {
+		return (T[]) myLinkedList.toArray();
+	}
 }
