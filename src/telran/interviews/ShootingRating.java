8ec1a0d8b039e9a2899b0617a5dac047d95232af
Daniel Kleyman <denny_klm1@yahoo.com>
2023-08-24 19:01:56 +0300

    ADD-13

---
+++ src/telran/interviews/ShootingRating.java
@@ -0,0 +1,25 @@
+package telran.interviews;
+
+import java.util.PriorityQueue;
+
+public class ShootingRating {
+
+	public static int rateShooting(int[] hittingResults) {
+		int resIndex = hittingResults.length - (int) (hittingResults.length * 0.7);
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		for (int e : hittingResults) {
+			pq.add(e);
+		}
+		Integer[] resArray = pq.toArray(new Integer[0]);
+		return resArray[resIndex];
+	}
+
+	public static void main(String[] args) {
+		int[] hittingResults = { 4, 3, 6, 7, 2, 9, 9, 4, 10, 8 };		
+		if (rateShooting(hittingResults) != 4) {
+			System.out.println("Test failed");
+			return;
+		}
+		System.out.println("Success");
+	}
+}
