966127b6e66c495322211825367c4e1c00cc8d38
Daniel Kleyman <denny_klm1@yahoo.com>
2023-08-31 16:49:06 +0300

    HW-28

---
+++ src/telran/interviews/test/ConnectionsPoolTests.java
@@ -0,0 +1,42 @@
+package telran.interviews.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.LinkedHashMap;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import telran.interviews.Connection;
+import telran.interviews.ConnectionsPool;
+
+class ConnectionsPoolTests {
+	ConnectionsPool map; 
+	Connection connection1 = new Connection(123, "abc", 1);
+	Connection connection2 = new Connection(124, "abcd", 2);
+	Connection connection3 = new Connection(125, "abcde", 3);
+	Connection connection4 = new Connection(126, "abcdef", 4);
+	@BeforeEach
+	void setUp() throws Exception {		
+		
+	map.addConnection(connection1);
+	map.addConnection(connection2);
+	map.addConnection(connection3);
+	
+	}
+
+	@Test
+	void addTest() {		
+		assertTrue(map.addConnection(connection4));
+		assertFalse(map.addConnection(connection4));
+		assertEquals(null, map.getConnection(connection1.getId()));
+	}
+	@Test
+	void getConnectionTest() {
+		assertEquals(null, map.getConnection(456));
+		assertEquals(connection1, map.getConnection(connection1.getId()));
+		map.addConnection(connection4);
+		assertEquals(null, map.getConnection(connection2.getId()));
+		
+	}
+}
