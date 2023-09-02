9e945e7fa44eac1dbab509869be77ccd15ee35dd
Daniel Kleyman <denny_klm1@yahoo.com>
2023-09-02 19:23:41 +0300

    HW-29

--- src/telran/interviews/InterviewTasks.java
+++ src/telran/interviews/InterviewTasks.java
@@ -1,23 +1,30 @@
 package telran.interviews;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class InterviewTasks {
-	
+
 	public record DateRole(LocalDate date, String role) {
-		
+
 	}
-	static public List<DateRole> rolesInDates(List<DateRole> datesRoles, List<LocalDate> dates){
+
+	static public List<DateRole> rolesInDates(List<DateRole> datesRoles, List<LocalDate> dates) {
+		List<DateRole> result = new ArrayList<>();
+		
+		for(int i = 0; i < dates.size(); i++) {
+			if(dates.get(0).isBefore(datesRoles.get(0).date)) {
 				
-		return datesRoles;
-		
+			}
+		}
+		return result;
+
 	}
-	 
-	        
+
 	static public void displayShuffled(int[] ar) {
 		Arrays.stream(ar).boxed().collect(Collectors.collectingAndThen(Collectors.toList(), list -> {
 			Collections.shuffle(list);
