38376881c1d9c73648dd3de0f024827204697c74
Daniel Kleyman <denny_klm1@yahoo.com>
2023-09-03 15:28:40 +0300

    HW-29

--- src/telran/interviews/WordsAutoCompletion.java
+++ src/telran/interviews/WordsAutoCompletion.java
@@ -2,7 +2,6 @@ package telran.interviews;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.TreeMap;
 
 public class WordsAutoCompletion implements Words {
