579988c05ca21c5e194cfbccfdb79c4b5f26ca7a
Daniel Kleyman <denny_klm1@yahoo.com>
2023-09-03 14:28:19 +0300

    HW-29

--- src/telran/interviews/WordsAutoCompletion.java
+++ src/telran/interviews/WordsAutoCompletion.java
@@ -1,19 +1,28 @@
 package telran.interviews;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class WordsAutoCompletion implements Words {
+	TreeMap<String, String> map = new TreeMap<String, String>();
 
 	@Override
 	public boolean addWord(String word) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean res = false;
+		if (!map.containsKey(word.toLowerCase())) {
+			map.put(word.toLowerCase(), word);
+			res = true;
+		}
+		return res;
 	}
 
 	@Override
-	public List<String> getWordsByPrefix(String prefix) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<String> getWordsByPrefix(String prefix) {		
+		Map<String, String> sorted = map.tailMap(prefix, true);
+		List<String> list = new ArrayList<String>(sorted.values());
+		return list;
 	}
 
 }
