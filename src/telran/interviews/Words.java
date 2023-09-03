09b921b5731209a813c405821a6002d4b390bd8f
Daniel Kleyman <denny_klm1@yahoo.com>
2023-09-03 13:23:08 +0300

    HW-29

---
+++ src/telran/interviews/Words.java
@@ -0,0 +1,9 @@
+package telran.interviews;
+
+import java.util.List;
+
+public interface Words {
+	boolean addWord(String word);
+
+	List<String> getWordsByPrefix(String prefix);
+}
---
+++ src/telran/interviews/WordsAutoCompletion.java
@@ -0,0 +1,19 @@
+package telran.interviews;
+
+import java.util.List;
+
+public class WordsAutoCompletion implements Words {
+
+	@Override
+	public boolean addWord(String word) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<String> getWordsByPrefix(String prefix) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
